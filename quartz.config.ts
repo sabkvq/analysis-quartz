c678792cd5d0005e45a40ca79b251683e40d0804
Bykov Sergei <SBykov@croc.ru>
2025-06-27 12:27:26 +0300

    start

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Академия ПМЭФ. Mind-Map",
+    pageTitle: "Анализ финансовых показателей компании",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
